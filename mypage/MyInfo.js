dcd51d9767290a79724cbc086be2e64256b1b98f
6feetlife <6feetlife56@gmail.com>
2025-03-22 16:06:04 +0900

    mainPage_UI_V.01

---
+++ mypage/MyInfo.js
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+function MyInfo () {
+    return(
+        <View>
+            <Text>email</Text>
+            <Text>dice@gmail.com</Text>
+            <Text>name</Text>
+            <Text>다이스</Text>
+            <Text>phone</Text>
+            <Text>010-1234-5678</Text>
+        </View>
+        
+    )
+}
+
+export default MyInfo;
--- signUp/LendingPage.js
+++ signUp/LendingPage.js
@@ -4,7 +4,7 @@ import LongButton from '../component/LongButton';
 import { FontAwesome } from '@expo/vector-icons'; // 아이콘 라이브러리
 //import EmailInput from './EmailInput';
 import LogoIcon from '../assets/icon/logo/logo_icon.svg'; // SVG를 React 컴포넌트처럼 사용
-import LogoText from '../assets/icon/logo/logo_dice Talk.svg';
+
 
 
 export default function LendingPage({navigation}) {
@@ -13,7 +13,6 @@ export default function LendingPage({navigation}) {
         <View style={styles.container}>
             <View style={styles.logoContainer}>
                 <LogoIcon width={200} height={200} />
-                <LogoText width={300} height={300} />
             </View>
 
             <View style={styles.bottomContainer}>
