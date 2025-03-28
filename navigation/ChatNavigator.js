5fc43a6fca5a4b3dabf221c35821ad1fde984681
YeoSeungA <silo1116@naver.cm>
2025-03-28 17:14:45 +0900

    HeartSend 이벤트

--- navigation/ChatNavigator.js
+++ navigation/ChatNavigator.js
@@ -16,8 +16,6 @@ export default function ChatNavigator() {
       <Stack.Screen name='ChatMain' component={ChatMain} />
       <Stack.Screen name='Chat' component={Chat} />
       <Stack.Screen name='ChatReport' component={ChatReport} />
-
-
       <Stack.Screen name='LetterEventScreen' component={LetterEventScreen} />
 
     </Stack.Navigator>
--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -9,11 +9,15 @@ import SelectAge from '../main/SelectAge';
 import DiceFriendsDs from '../main/DiceFriendsDs';
 import hihihi from '../main/hihihi';
 
+
 import LetterEventScreen from '../loveEvent/LetterEventScreen';
 
 import DropOutMember from '../plus/DropOutMember';
 import CheckDropOutMember from '../plus/CheckDropOutMember';
 import LendingPage from '../signUp/LendingPage';
+
+import LetterEventScreen from '../loveEvent/LetterEventScreen';
+
 
 
 const Stack = createNativeStackNavigator();
@@ -28,29 +32,6 @@ export default function MainNavigator() {
       <Stack.Screen name='DiceFriendsDs' component={DiceFriendsDs} />
       <Stack.Screen name='hihihi' component={hihihi} />
 
-
-      {/* <Stack.Screen name='LetterEventScreen' component={LetterEventScreen} /> */}
-
-      <Stack.Screen 
-        name='DropOutMember' 
-        component={DropOutMember}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen 
-        name='CheckDropOutMember' 
-        component={CheckDropOutMember}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen 
-        name='LendingPage' 
-        component={LendingPage}
-        options={{ headerShown: false }}
-      />
-
-
-      <Stack.Screen name='LetterEventScreen' component={LetterEventScreen} />
-
-
       <Stack.Screen name='LetterEventScreen' component={LetterEventScreen} />
 
       <Stack.Screen 
@@ -68,8 +49,6 @@ export default function MainNavigator() {
         component={LendingPage}
         options={{ headerShown: false }}
       />
-
-
     </Stack.Navigator>
   );
 } 
