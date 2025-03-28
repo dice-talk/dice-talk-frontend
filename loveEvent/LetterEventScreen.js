6f99f56278aecea16c2e4428bc67d47186b32ad2
YeoSeungA <silo1116@naver.cm>
2025-03-28 16:19:16 +0900

    Heart 보내기 이벤트

--- loveEvent/LetterEventScreen.js
+++ loveEvent/LetterEventScreen.js
@@ -2,19 +2,33 @@ import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import LetterRain from '../component/LetterRain';
 import LoveNoteCard from '../component/LoveNoteCard';
+import HeartVoteModal from './HeartVoteModal';
 
 export default function LetterEventScreen() {
     const [showCard, setShowCard] = useState(false);
+    const [isVoteModalVisible, setVoteModalVisible] = useState(true); 
 
     const handleSend = (text) => {
         console.log('보낸 편지 내용:', text);
     };
 
     return (
+        <>
+        <HeartVoteModal
+            visible={isVoteModalVisible}
+            onSelectDice={(id) => {
+                console.log('선택된 상대 ID:', id);
+            setVoteModalVisible(false); // 모달닫기 -> 이후 LetterRain 실행행
+        }}
+        onClose={() => setVoteModalVisible(false)} />
+
+        {!isVoteModalVisible && (
         <View style={styles.container}>
             {!showCard && <LetterRain onFinish={() => setShowCard(true)} />}
             {showCard && <LoveNoteCard onSubmit={handleSend} />}
         </View>
+        )}
+        </>
     );
 };
 
